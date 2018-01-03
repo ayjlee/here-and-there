f17390c472214d3a32c3875a5599fc0dd766c8f5
Amy Lee <amy.yj.lee@gmail.com>
2018-01-03 11:32:18 -0800

    used npm library google-maps-react to lazily load Google Maps API into my app, set up env variables to load in google maps api key, currently displaying map on page that is not styled to test that it is working

--- here-and-there/src/App.js
+++ here-and-there/src/App.js
@@ -1,21 +1,39 @@
+import {Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">here and there</h1>
+//         </header>
+//         <p className="App-intro">
+//           maps!
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+export class MapContainer extends Component {
+render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Map google={this.props.google} zoom={14}>
+
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+      </Map>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GMAPI_KEY,
+  libraries: ['places', 'visualization'],
+})(MapContainer);
