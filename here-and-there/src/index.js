9f68a89ce2513cefb0c7670bb4de02e1aedd0624
Amy Lee <amy.yj.lee@gmail.com>
2018-01-03 14:00:12 -0800

    added prop-types library

--- here-and-there/src/index.js
+++ here-and-there/src/index.js
@@ -1,8 +1,10 @@
+import {Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import CurrentMap from './CurrentMap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CurrentMap />, document.getElementById('root'));
 registerServiceWorker();
