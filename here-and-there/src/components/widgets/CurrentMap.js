aca6a04f658c6dbe53da13765abbed1070fd0f6e
Amy Lee <amy.yj.lee@gmail.com>
2018-01-06 14:54:56 -0800

    created custom GM component to fix styling issues

--- here-and-there/src/components/widgets/CurrentMap.js
+++ here-and-there/src/components/widgets/CurrentMap.js
@@ -1,149 +1,53 @@
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { GoogleApiWrapper } from 'google-maps-react';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export class CurrentMap extends React.Component {
-  // adding constructor to make Map objects stateful(have state)
-  constructor(props) {
-    super(props);
-
-    const {lat, lng} = this.props.initialCenter;
-    this.state = {
-      currentLocation: {
-        lat: lat,
-        lng: lng,
-      },
-    };
-  }
-  // componentDidMount handles case when Map is already available when component mount, eg. when map has been previously loaded in the app
   componentDidMount() {
-    // setup callback to fetch the current position
-    if (this.props.centerAroundCurrentLocation) {
-      // navigator is from native browser implementation
-      if (navigator && navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((pos) => {
-          const coords = pos.coords;
-          this.setState({
-            currentLocation: {
-              lat: coords.latitude,
-              lng: coords.longitude,
-            },
-          });
-        });
-      }
-    }
     this.loadMap();
   }
   componentDidUpdate(prevProps, prevState) {
-    // check if Map component is loaded, loadMap if true
+    console.log('in componentDidUpdate');
     if (prevProps.google !== this.props.google) {
+      console.log('in componentDidUpdate');
       this.loadMap();
     }
-    if (prevState.currentLocation !== this.state.currentLocation) {
-      this.recenterMap();
-    }
-  }
-  recenterMap() {
-    // use panTo() to change the center of Map
-    const map = this.map;
-    const curr = this.state.currentLocation;
-
-    const google = this.props.google;
-    const maps = google.maps;
-
-    if (map) {
-      let center = new maps.LatLng(curr.lat, curr.lng);
-      map.panTo(center);
-    }
-
   }
   loadMap() {
-    // function to load the map
+    console.log('in loadMap');
     if (this.props && this.props.google) {
-      // if google is available ^
-      const { google } = this.props;
-      const maps = google.maps
-
+      // google is available
+      console.log('google is available');
+      const {google} = this.props;
+      const maps = google.maps;
       const mapRef = this.refs.map;
-      // findDOMNode gives warning, try using callback refs instead
-        // const node = ReactDOM.findDOMNode(mapRef);
-      // using callbackRefs:
-      const node = this.node;
-      let { initialCenter, zoom } = this.props;
-      const { lat, lng } = this.state.currentLocation;
-      const center = new maps.LatLng(lat, lng);
+      const node= ReactDOM.findDOMNode(mapRef);
+      console.log('node is:');
+      console.log(node);
+      let zoom = 14;
+      let lat = 47.6062;
+      let lng = -122.3321;
+      const center = new maps.LatLng(lat,lng);
       const mapConfig = Object.assign({}, {
         center: center,
-        zoom: zoom,
-      });
-      // maps.Map() constructor accepts a DOM node and a configuration object to create a map. To instantiate a map, we need at least two config options: center, and zoom
+        zoom: zoom
+      })
+      this.map = new maps.Map(node, mapConfig);
+      this.forceUpdate();
     }
-  }
-  renderChildren() {
-    const { children } = this.props;
-    // return null if there are no children passed to Map instance
-    if (!children) return;
-
-    return React.Children.map(children, (c) => {
-      return React.cloneElement(c, {
-        map: this.map,
-        google: this.props.google,
-        mapCenter: this.state.currentLocation,
-      });
-    });
   }
   render() {
-    // JSX below
-      // return (
-      // // original 'ref' way
-      // // <div ref = 'map'>
-      // //   Loading map...
-      // //   {this.renderChildren()}
-      // // </div>
-      // // trying to use callbacks:
-      // // renderChildren() will be responsible for calling the methods on the children. we'll use React.cloneElement() to add props to child inside component and append the map instance, google prop, and map center
-      //   <div ref = {node => this.node = node} >
-      //     Loading map...
-      //     {this.renderChildren()}
-      //   </div>
-      // );
-    if (!this.props.loaded) {
-      return <div>Loading...</div>
-    }
-
+    const style = {
+      with: '75vw',
+      height: '75vh',
+    };
     return (
-      <Map google={this.props.google}
-        style={{width: '80%', height: '80%', position: 'relative'}}
-        className={'map'}
-        zoom={14}>
-        <Marker
-          title={'The marker`s title will appear as a tooltip.'}
-          name={'SOMA'}
-          position={{lat: 37.778519, lng: -122.405640}} />
-        <Marker
-          name={'Dolores park'}
-          position={{lat: 37.759703, lng: -122.428093}} />
-        <Marker />
-      </Map>
-    )
+      <div ref="map" style= {style}>
+        The CurrentMap will load here
+      </div>
+    );
   }
 }
-
-CurrentMap.propTypes = {
-  google: PropTypes.object,
-  zoom: PropTypes.number,
-  initialCenter: PropTypes.object,
-  centerAroundCurrentLocation: PropTypes.bool,
-};
-CurrentMap.defaultProps = {
-  zoom: 13,
-  // setting Seattle as default map location
-  initialCenter: {
-    lat: 47.6062,
-    lng: -122.3321,
-  },
-  centerAroundCurrentLocation: false,
-};
 
 export default CurrentMap;
--- here-and-there/src/components/widgets/MapContainer.js
+++ here-and-there/src/components/widgets/MapContainer.js
@@ -1,30 +1,26 @@
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import {GoogleApiWrapper } from 'google-maps-react';
 import React, { Component } from 'react';
 import CurrentMap from './CurrentMap';
 
-// const { REACT_APP_GMAPI_KEY } = process.env;
-
-export class MapContainer extends Component {
+export class MapContainer extends React.Component {
   render() {
     const style = {
-      width: '300px',
-      height: '300px',
+      width: '80vw',
+      height: '80vh',
     }
-    const pos = { lat: 37.759703, lng: -122.428093 };
+    if (!this.props.loaded) {
+      return <div>Loading Map Container...</div>
+    }
+    console.log('this.props.google is:');
+    console.log(this.props.google);
     return (
-      <div className= "map-container2" style={style}>
-        <Map google={this.props.google} initialCenter={{
-            lat: 47.6062,
-            lng: -122.3321
-          }}>
-          <Marker / >
-          <Marker position= {pos} />
-        </Map>
+      <div>
+        <CurrentMap google= {this.props.google}/>
       </div>
     )
   }
 }
-console.log(process.env.REACT_APP_GMAPI_KEY);
+
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_GMAPI_KEY,
   libraries: ['places', 'visualization'],
